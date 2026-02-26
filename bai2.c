a90857291cfa5db0f4b86bd7b1973b382a8bafc1
hnhatkhang02122007-beep <hnhatkhang02122007@gmail.com>
2026-02-26 09:49:27 +0700

    khangnopbai

--- bai2.c
+++ bai2.c
@@ -2,36 +2,43 @@
 
 int main() {
     int n, i;
-    
-    // 1. Input the size of the array
+
+    // Bước 1: Nhập số lượng phần tử n
     printf("Nhap so luong phan tu n: ");
     scanf("%d", &n);
-    
-    int a[n]; // Declaring array of size n
 
-    // 2. Input array elements
-    for(i = 0; i < n; i++) {
+    // Kiểm tra điều kiện n để tránh lỗi bộ nhớ
+    if (n <= 0) {
+        printf("So phan tu phai lon hon 0!\n");
+        return 1;
+    }
+
+    int a[n]; // Khai báo mảng có n phần tử
+
+    // Bước 2: Nhập các phần tử của mảng
+    for (i = 0; i < n; i++) {
         printf("Nhap a[%d]: ", i);
         scanf("%d", &a[i]);
     }
 
-    // 3. Initialize max and min with the first element
+    // Bước 3: Giả sử phần tử đầu tiên là Max và Min
     int max = a[0];
     int min = a[0];
 
-    // 4. Loop to compare and find min/max
-    for(i = 1; i < n; i++) {
-        if(a[i] > max) {
-            max = a[i];
+    // Bước 4: Duyệt mảng từ phần tử thứ 2 để tìm Max, Min thực sự
+    for (i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i]; // Cập nhật Max nếu tìm thấy số lớn hơn
         }
-        if(a[i] < min) {
-            min = a[i];
+        if (a[i] < min) {
+            min = a[i]; // Cập nhật Min nếu tìm thấy số nhỏ hơn
         }
     }
 
-    // 5. Output results
-    printf("\nGia tri lon nhat: %d", max);
-    printf("\nGia tri nho nhat: %d\n", min);
+    // Bước 5: In kết quả ra màn hình
+    printf("\n--- KET QUA ---");
+    printf("\nGia tri lon nhat (Max) la: %d", max);
+    printf("\nGia tri nho nhat (Min) la: %d\n", min);
 
     return 0;
 }
